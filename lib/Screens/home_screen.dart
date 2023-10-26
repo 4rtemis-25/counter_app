09a2c4107ef3b4eb253d3ac1d2ebe0b728d14783
Johan Hdez <hernandez.gomez29202@gmail.com>
2023-10-25 20:59:15 -0500

    Con floatingButton

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    const fontSize = TextStyle( fontSize: 30 );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('GMJ Hogar Mobile'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 149, 10, 0),
+        elevation: 0,
       ),
       body: const Center(
-        child: Text('GMJ Hogar'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Text('Número De Clicks', style: fontSize,),
+            Text('10', style: fontSize,),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: (){
+          print('Hola Mundo');
+        }),
     );
   }
-
 }
